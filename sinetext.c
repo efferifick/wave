9600f7750a02464744dc1f8eb383329b827cf918
Erick Eduardo Ochoa Lopez <eochoa@ualberta.ca>
2013-07-08 00:35:56 -0500

    Uses getopt_long to parse through options.

--- sinetext.c
+++ sinetext.c
@@ -2,25 +2,81 @@
 #include <stdlib.h>
 #include <math.h>
 #include <unistd.h>
+#include <getopt.h>
 
 #ifndef M_PI
 #define M_PI (3.141592654)
 #endif
 
+const static char* HELP_STR = "sinetext #_of_samples" 
+				"#_of_frequency_in_Hz #_of_sample_rate";
+const static char* VERSION_STR = "sinetext version 0.01";
+
+static int verbose = 0;
+
+/*
+ * you need... sample, freq, samplerate...
+ *
+ */
 int
 main(int argc, char** argv)
 {
-	int i, n_samples;
-	double sample;
+	int c;
+	int optindex =  0;
+	int sample_size; /* arg1 */
 	const double TWO_PI = 2*M_PI;
+	double frequency; /* arg2 */
+	double sample;
+	double sample_rate; /* arg3 */
 	double angleincr;
-	n_samples = 50;
-	angleincr = TWO_PI / n_samples;
-	for (i = 0; i < n_samples; i++) 
+	
+
+	const struct option long_opts[] = 
+	{
+		{"verbose", no_argument, &verbose, 1},
+		{"version", no_argument, 0, 'v'},
+		{"help", no_argument, 0, 'h'},
+		{0, 0, 0, 0}
+	};
+
+	while ((c = getopt_long(argc, argv, "vh", long_opts, &optindex)) != -1)
+	{
+		switch(c)
+		{
+			case 0:
+			break;
+			
+			case 'h':
+			printf("%s\n", HELP_STR);
+			exit(EXIT_SUCCESS);
+			break;
+
+			case 'v':
+			printf("%s\n", VERSION_STR);
+			exit(EXIT_SUCCESS);
+			break;
+
+			default:
+			printf("%s\n", HELP_STR);
+			exit(EXIT_FAILURE);
+		}	
+	}
+
+	if ((argc - optind) != 3)
+	{
+		printf("%s\n", HELP_STR);
+		exit(EXIT_FAILURE);
+	}
+
+	sample_size = atoi(argv[optind++]);
+	frequency = atof(argv[optind++]);
+	sample_rate = atof(argv[optind]);
+	angleincr = TWO_PI * frequency / sample_rate;
+
+	for (int i = 0; i < sample_size; i++) 
 	{
 		sample = sin(angleincr*i);
-		printf("%.8lf\n", sample);
+		printf("%.8f\n", sample);
 	}
-	fprintf(stderr, "done\n");
 	exit(EXIT_SUCCESS);
 }
