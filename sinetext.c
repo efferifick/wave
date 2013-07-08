1c340d66c4dafbc6e23bff5f7d788030ee6025cb
Erick Eduardo Ochoa Lopez <eochoa@ualberta.ca>
2013-07-07 21:24:48 -0500

    First commit

---
+++ sinetext.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+
+#ifndef M_PI
+#define M_PI (3.141592654)
+#endif
+
+int
+main(int argc, char** argv)
+{
+	exit(EXIT_SUCCESS);
+}
